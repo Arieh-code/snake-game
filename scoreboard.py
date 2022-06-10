409bd6a555750fc02a7659850d3233c146caa07c
Arieh Norton <arieh.norton@gmail.com>
2022-06-10 15:55:10 +0300

    updated data logic

--- scoreboard.py
+++ scoreboard.py
@@ -7,6 +7,8 @@ FONT = ("Courier", 24, "normal")
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        with open("data.txt") as file:
+            self.high_score = int(file.read())
         self.score = 0
         self.penup()
         self.goto(0, 260)
@@ -15,13 +17,21 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_score(self):
-        self.write(f"Score : {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def add_score(self):
         self.score += 1
-        self.clear()
         self.update_score()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", "w") as file:
+                file.write(f"{self.high_score}")
+            self.score = 0
+            self.update_score()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align=ALIGNMENT, font=FONT)
